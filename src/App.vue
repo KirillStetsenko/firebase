5ae771600c976679f99fb8fb145cdb1d60967b47
Kirill <151939358+KirillStetsenko@users.noreply.github.com>
2025-03-12 22:16:38 +0200

    deploy firebase with github CI/CD

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="h-screen bg-gradient-to-b from-blue-900 to-violet-900 text-white">
+    <h1 class="text-center text-3xl">firebase deploy from github Actions</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
